7e472431f9e0c522cf57a4a0cdf697adb7846188
cortins-05 <lucasortins@gmail.com>
2025-05-29 11:48:13 +0200

    cambios

---
+++ aprendiendo-php/11-Ejercicios/Ejercicio6.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_GET['numero1'])&&isset($_GET['numero2'])){
+    $numero1 = $_GET['numero1'];
+    $numero2 = $_GET['numero2'];
+    for($i=$numero1+1;$i<$numero2;$i++){
+        if($i%2!=0){
+            echo "$i, "; 
+        }   
+    }
+}else{
+    echo '<h1>Introduce correctamente los numeros</h1>';
+    echo '<form action="Ejercicio6.php" method="GET" >
+            <label>Numero 1:</label> <input type="text" name="numero1" id="">
+            <label>Numero 2:</label> <input type="text" name="numero2" id="">
+            <input type="submit" value="Enviar">
+        </form>';
+}
+
+?>
