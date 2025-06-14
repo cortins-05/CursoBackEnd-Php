5ed2ec983f1cb6645f417cac7e3d95043a22b24d
cortins-05 <lucasortins@gmail.com>
2025-06-14 18:55:34 +0200

    cambios

---
+++ aprendiendo-php/18-Formularios/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios</title>
+</head>
+<body>
+
+    <h1>Formulario</h1>
+    <form action="" method="POST" >
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" placeholder="Introduce tu nombre" autofocus required> <br>
+        <label for="apellido">Apellido:</label>
+        <input type="text" name="apellido" required> <br>
+        <input type="submit" value="Enviar">
+    </form>
+
+</body>
+</html>
