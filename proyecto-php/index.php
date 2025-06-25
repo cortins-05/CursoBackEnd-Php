7cb87ffaf90937c45641afc5f836dae45714fb89
cortins-05 <lucasortins@gmail.com>
2025-06-25 11:30:22 +0200

    cambios tochos

---
+++ proyecto-php/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog de Videojuegos</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <!-- Cabezera -->
+    <header id="cabezera">
+        <!-- LOGO -->
+        <div id="logo">
+            <a href="index.php">
+                Blog de Videojuegos
+            </a>
+        </div>
+    
+    
+    <!-- MENU -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="index.php">Inicio</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="index.php">Sobre mi</a>
+                </li>
+                <li>
+                    <a href="index.php">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <div id="contenedor">
+        <!-- BARRA LATERAL -->
+        <aside id="sidebar">
+            <div id="login" class="bloque">
+                <h3>Identificate</h3>
+                <form action="login.php" method="POST">
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+
+                    <label for="contraseña">Contraseña</label>
+                    <input type="password" name="contraseña">
+
+                    <input type="submit" value="Entrar">
+                </form>
+            </div>
+
+            <div id="register" class="bloque">
+                <h3>Registrarse</h3>
+                <form action="registro.php" method="POST">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre">
+                    
+                    <label for="apellidos">Apellidos</label>
+                    <input type="text" name="apellidos">
+
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+
+                    <label for="contraseña">Contraseña</label>
+                    <input type="password" name="contraseña">
+
+                    <input type="submit" value="Registrar">
+                </form>
+            </div>
+        </aside>
+
+        <!-- CAJA PRINCIPAL -->
+        <div id="principal">
+            <h1>Ultimas entradas</h1>
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, hic odit error mollitia nam vel repudiandae amet nobis consequuntur facere? Voluptate magni adipisci, hic veniam porro sit tenetur enim et?
+                </p>
+            </article>
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, hic odit error mollitia nam vel repudiandae amet nobis consequuntur facere? Voluptate magni adipisci, hic veniam porro sit tenetur enim et?
+                </p>
+            </article>
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, hic odit error mollitia nam vel repudiandae amet nobis consequuntur facere? Voluptate magni adipisci, hic veniam porro sit tenetur enim et?
+                </p>
+            </article>
+        </div>
+
+    </div>
+    <!-- PIE DE PAGINA -->
+    <footer id="pie">
+        <p>Desarrollado por Lucas Ortins Méndez &copy; 2018</p>
+    </footer>
+</body>
+</html>
