27beb7121d6fe7f588f5ad8fd40650640ee49e48
cortins-05 <lucasortins@gmail.com>
2025-07-01 18:21:17 +0200

    cambios tochos

---
+++ proyecto-php/categoria.php
@@ -0,0 +1,38 @@
+<?php require_once("includes/cabecera.php"); ?>
+<?php require_once "includes/lateral.php"; ?>
+
+<?php
+    $categoria_actual = conseguirCategoria($db,$_GET['id']);
+    if(!isset($categoria_actual['id'])){
+        header("Location:index.php");
+    }
+?>
+
+<div id="principal">
+            <h1>Entradas de <?=$categoria_actual['nombre']?></h1>
+            <?php
+                $entradas = conseguirEntradas($db,null,$_GET['id']);
+                if(!empty($entradas)):
+                    while($entrada=mysqli_fetch_assoc($entradas)): 
+            ?>
+                        <article class="entrada">
+                            <a href="">
+                                <h2><?=$entrada['titulo']?></h2>
+                                <span class="fecha"><?=$entrada['categoria'].' | '.$entrada['fecha']?></span>
+                                <p>
+                                    <?=substr($entrada['descripcion'],0,180)."..."?>
+                                </p>
+                            </a>
+                        </article>
+            <?php
+                    endwhile;
+                else:
+            ?>
+            <div class="alerta">No hay entradas para esta categoría</div>
+            <?php endif; ?>
+
+            <div id="ver-todas">
+                <a href="entradas.php">Ver todas las entradas</a>
+            </div>
+
+<?php require_once("includes/pie.php"); ?>
--- proyecto-php/includes/helpers.php
+++ proyecto-php/includes/helpers.php
@@ -29,6 +29,17 @@ function conseguirCategorias($db){
     return false;
 }
 
+function conseguirCategoria($db,$id){
+    $sql = "select * from categorias where id=$id;";
+    $categorias = mysqli_query($db,$sql);
+    $result = array();
+    if($categorias&&mysqli_num_rows($categorias)>=1){
+       $result = mysqli_fetch_assoc($categorias);
+       return $result;
+    }
+    return false;
+}
+
 function conseguirUltimasEntradas($conexion){
     $sql = "Select e.*, c.nombre as 'categoria' from entradas e inner join categorias c on e.categoria_id = c.id order by e.id desc limit 4";
     $entradas = mysqli_query($conexion,$sql);
@@ -39,8 +50,12 @@ function conseguirUltimasEntradas($conexion){
     return $resultado;
 }
 
-function conseguirEntradas($conexion,$limit=null){
-    $sql = "Select e.*, c.nombre as 'categoria' from entradas e inner join categorias c on e.categoria_id = c.id order by e.id desc";
+function conseguirEntradas($conexion,$limit=null,$categoria = null){
+    $sql = "Select e.*, c.nombre as 'categoria' from entradas e inner join categorias c on e.categoria_id = c.id";
+    if(!empty($categoria)){
+        $sql .= " where e.categoria_id = $categoria";
+    }
+    $sql .= " order by e.id desc";
     if($limit!=null){
         $sql .= " limit $limit";
     }
