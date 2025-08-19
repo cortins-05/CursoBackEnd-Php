a28457a9d25100153ee9e03c743d66b381b998ef
cortins-05 <lucasortins@gmail.com>
2025-08-19 14:58:26 +0200

    cambios

--- aprendiendo-php-poo/12-Autoload/index.php
+++ aprendiendo-php-poo/12-Autoload/index.php
@@ -12,7 +12,13 @@ require_once 'autoload.php';
 // echo $categoria->nombre;
 // echo '<br/>';
 
+
+// ESPACIOS DE NOMBRES Y PAQUETES
 use MisClases\Usuario;
+use MisClases\Categoria;
+use MisClases\Entrada;
+use PanelAdministrador\Usuario as UsuarioAdmin;
+
 class Principal{
     public $usuario;
     public $categoria;
@@ -20,9 +26,59 @@ class Principal{
 
     public function __construct(){
         $this->usuario = new Usuario();
-        $this->categoria = new MisClases\Categoria();
-        $this->entrada = new MisClases\Entrada();
+        $this->categoria = new Categoria();
+        $this->entrada = new Entrada();
     }
+
+    public function getUsuario(){
+        return $this->usuario;
+    }
+
+    public function getCategoria(){
+        return $this->categoria;
+    }
+
+    public function getEntrada(){
+        return $this->entrada;
+    }
+
+    public function setUsuario($usuario){
+        $this->usuario = $usuario;
+    }
+
+    public function setCategoria($categoria){
+        $this->categoria = $categoria;
+    }
+
+    public function setEntrada($entrada){
+        $this->entrada = $entrada;
+    }
+
+    function informacion(){
+        echo __NAMESPACE__;
+    }
+
+}
+
+//Objeto Principal
+$principal = new Principal();
+$principal->informacion();
+//var_dump($principal->usuario);
+$metodos = get_class_methods($principal);
+
+$busqueda = array_search('setApellidos', $metodos);
+var_dump($busqueda);
+
+//Objeto otro paquete
+$usuario = new UsuarioAdmin();
+$usuario->informacion();
+
+// Comprobar si existe una clase
+$clase = class_exists('PanelAdministrador\Usuario');
+if($clase){
+    echo "<h1>La clase SI existe</h1>";
+}else{
+    echo "<h1>La clase NO existe</h1>";
 }
 
 ?>
