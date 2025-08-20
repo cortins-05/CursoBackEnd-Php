8463d3bd9ff0864645ab7676ed5728ab62b08465
cortins-05 <lucasortins@gmail.com>
2025-08-21 00:21:32 +0200

    cambios

---
+++ aprendiendo-php-mvc/index.php
@@ -0,0 +1,22 @@
+<h1>Bienvenido a mi web con MVC</h1>
+<?php
+require_once 'controllers/usuario.php';
+
+$controlador = new UsuarioController;
+
+if(isset($_GET['controller']) && class_exists($_GET['controller'])){
+    $nombre_controlador = $_GET['controller'];
+    $controlador = new $nombre_controlador();
+        if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+        $action = $_GET['action'];
+        $controlador->$action();
+    }else{
+        echo '<p>La pagina que buscas no existe</p>';
+    }
+}else{
+    echo '<p>La pagina que buscas no existe</p>';
+}
+
+
+
+?>
