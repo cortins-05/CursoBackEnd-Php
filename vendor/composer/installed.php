d860a801053c55649c1a2d4fd0f26e58b4e892d3
cortins-05 <lucasortins@gmail.com>
2025-08-22 13:34:09 +0200

    cambios

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6b88fd56d34eae82e4d27f47f925c9c3e104d8c6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6b88fd56d34eae82e4d27f47f925c9c3e104d8c6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'masterexploder/phpthumb' => array(
+            'pretty_version' => '2.1',
+            'version' => '2.1.0.0',
+            'reference' => '01f6aa65aa3d136e175f67da2b4ea3e316770d02',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../masterexploder/phpthumb',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
