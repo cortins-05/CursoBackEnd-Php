755658fd789e73d351f2f67bd84ac0e3f0e331b8
cortins-05 <lucasortins@gmail.com>
2025-08-22 16:32:28 +0200

    cambios

---
+++ proyecto-php-poo/controllers/ProductoController.php
@@ -0,0 +1,10 @@
+<?php
+
+class ProductoController{
+    public function index(){
+        //Renderizar vista
+        require_once 'views/producto/destacados.php';
+    }
+}
+
+?>
---
+++ proyecto-php-poo/views/layout/footer.php
@@ -0,0 +1,11 @@
+</div>
+    </div>
+
+        <!--Pie de pagina-->
+
+        <footer id="footer">
+            <p>Desarrollado por Lucas Ortins &copy; <?=date('Y')?></p>
+        </footer>
+    </div>
+</body>
+</html>
