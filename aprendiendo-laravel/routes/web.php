87846ca0f05067e3dc64a783cdd25551f65fc3ec
cortins-05 <lucasortins@gmail.com>
2025-09-24 16:07:14 +0200

    changes

--- aprendiendo-laravel/routes/web.php
+++ aprendiendo-laravel/routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/*
+GET: Conseguir datos
+POST: Guardar datos
+PUT: Actualizar recursos
+DELETE: Eliminar recursos 
+*/
+
+Route::get('/mostrar-fecha',function(){
+    $titulo = "Estoy mostrando la fecha";
+    return view('mostrar-fecha', array(
+        'titulo'=>$titulo
+    ));
+});
+
+Route::get('/pelicula/{titulo?}',function($titulo='No hay una pelicula seleccionada'){
+    return view('pelicula',array(
+        'titulo'=>$titulo
+    ));
+});
